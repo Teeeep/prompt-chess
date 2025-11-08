72b7693625d3e167e85ca48b690675066102f041
Casper Tepe <casperjantepe@gmail.com>
2025-11-08 12:54:53 +0100

    fix: Simplify VCR configuration and re-record cassettes
    - Removed problematic before_record hook that was filtering keys before requests
    - Now using only filter_sensitive_data which filters after successful requests
    - Re-recorded cassettes with real API calls
    - Manually filtered API keys from cassettes using sed
    
    Some cassettes still have bad 401 responses from previous recording attempts and need to be deleted and re-recorded.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,23 +5,9 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
 
-  # Filter actual API keys from ENV
-  c.filter_sensitive_data('<OPENAI_API_KEY>') { ENV['OPENAI_API_KEY'] }
-  c.filter_sensitive_data('<ANTHROPIC_API_KEY>') { ENV['ANTHROPIC_API_KEY'] }
-
-  # Filter all Anthropic API keys (including test keys) by pattern
-  c.before_record do |interaction|
-    api_key_header = interaction.request.headers['X-Api-Key']&.first
-    if api_key_header&.start_with?('sk-ant-')
-      interaction.request.headers['X-Api-Key'] = ['<ANTHROPIC_API_KEY>']
-    end
-
-    # Filter OpenAI keys from Authorization header
-    auth_header = interaction.request.headers['Authorization']&.first
-    if auth_header&.include?('Bearer sk-')
-      interaction.request.headers['Authorization'] = ['Bearer <OPENAI_API_KEY>']
-    end
-  end
+  # Filter actual API keys from ENV (only if set)
+  c.filter_sensitive_data('<OPENAI_API_KEY>') { ENV['OPENAI_API_KEY'] } if ENV['OPENAI_API_KEY']
+  c.filter_sensitive_data('<ANTHROPIC_API_KEY>') { ENV['ANTHROPIC_API_KEY'] } if ENV['ANTHROPIC_API_KEY']
 
   c.allow_http_connections_when_no_cassette = false
 
