64d16fa93af30b923c7b1c1b22bb45402dc98718
Casper Tepe <casperjantepe@gmail.com>
2025-11-08 13:29:02 +0100

    fix: Configure counter_cache to use total_moves column
    - Changed Move model to use counter_cache: :total_moves
    - Removed moves_count column as it's no longer needed
    - Removed total_moves method override in Match model
    - Counter cache now properly updates total_moves automatically
    - Fixes Types::MatchType test expecting total_moves to reflect move count

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_08_121920) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_08_122517) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -41,7 +41,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_08_121920) do
     t.text "error_message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "moves_count", default: 0, null: false
     t.index ["agent_id"], name: "index_matches_on_agent_id"
     t.index ["created_at"], name: "index_matches_on_created_at"
     t.index ["status"], name: "index_matches_on_status"
